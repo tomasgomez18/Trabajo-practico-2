46f299b0ac1ef63cfe9e969f7c9de875d699f6d7
tomas <tomasgomezzi36@gmail.com>
2025-08-12 22:51:58 -0300

    first exercise completed

---
+++ ej1.js
@@ -0,0 +1,10 @@
+// Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+
+let edad = parseInt(prompt("Ingrese su edad"))
+
+if(edad>=18){
+    alert("Su edad es apta para conducir")
+}else{
+    alert("Ingrese un caracte valido o usted es menor de edad")
+    }
