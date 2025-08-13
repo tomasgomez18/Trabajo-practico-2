859a5609608fed9358976762433f8fac888f714c
tomas <tomasgomezzi36@gmail.com>
2025-08-13 00:51:43 -0300

    third exercise completed

---
+++ ej3.js
@@ -0,0 +1,17 @@
+// Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+let cadena = " ";
+do {
+  let texto = prompt("Ingrese una cadena de texto").toLowerCase();
+  if (cadena == "") {
+    cadena = texto;
+  } else {
+    cadena += "" + texto;
+  }
+  let confirmacion = confirm("Quiere seguir ingresando");
+
+  if (!confirmacion) {
+    break;
+  }
+} while (true);
+alert(`textos concatenados + ${cadena}`)
